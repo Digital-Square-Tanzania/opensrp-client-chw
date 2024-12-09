ea50d51eac5c44b41c2a08e6a40ba3588abeca9f
cozej4 <ilakozejumanne@gmail.com>
2024-12-09 20:00:42 +0300

    :hammer: Fixed build issues

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/PrivacyPolicyActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/PrivacyPolicyActivity.java
@@ -5,28 +5,36 @@ import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 import org.smartregister.chw.R;
 
 public class PrivacyPolicyActivity extends AppCompatActivity {
+
     private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle("Privacy Policy");
         }
+
         webView = findViewById(R.id.webView);
+
         // Configure WebView settings
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);  // Enable JavaScript (if needed)
+
         // Load URL in the WebView
         webView.setWebViewClient(new WebViewClient());  // Ensures links open in WebView
-        webView.loadUrl("https://nacp.go.tz/privacy-policy/");
+        webView.loadUrl("https://ucs.nacp.go.tz/privacy-policy/");
     }
+
     @Override
     public void onBackPressed() {
         if (webView.canGoBack()) {
@@ -35,6 +43,7 @@ public class PrivacyPolicyActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
     // Handle the Up button click to finish the activity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -44,4 +53,5 @@ public class PrivacyPolicyActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
