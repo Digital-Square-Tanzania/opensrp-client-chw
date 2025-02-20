9e9740612566d5d38629ae4a8f931af63eaecdf9
cozej4 <ilakozejumanne@gmail.com>
2025-02-20 11:22:02 +0300

    :hammer: Refactored Name of HPS household and client services fragments

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/HpsHouseholdRegisterFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/HpsHouseholdRegisterFragment.java
@@ -12,10 +12,23 @@ import org.smartregister.cursoradapter.RecyclerViewPaginatedAdapter;
 import org.smartregister.family.R;
 import org.smartregister.family.util.DBConstants;
 import org.smartregister.family.util.Utils;
+import org.smartregister.view.customcontrols.CustomFontTextView;
+import org.smartregister.view.customcontrols.FontVariant;
 
 import java.util.Set;
 
 public class HpsHouseholdRegisterFragment extends CoreHpsRegisterFragment {
+    @Override
+    public void setupViews(android.view.View view) {
+        super.setupViews(view);
+        CustomFontTextView titleView = view.findViewById(org.smartregister.chw.hps.R.id.txt_title_label);
+        if (titleView != null) {
+            titleView.setVisibility(android.view.View.VISIBLE);
+            titleView.setText(getString(org.smartregister.chw.hps.R.string.hps_household_register));
+            titleView.setFontVariant(FontVariant.REGULAR);
+        }
+    }
+
     @Override
     public void initializeAdapter(Set<View> visibleColumns) {
         CoreRegisterProvider chwRegisterProvider = new FamilyRegisterProvider(getActivity(), commonRepository(), visibleColumns, registerActionHandler, paginationViewHandler);
