39df94eb0726e2754eae711558e125007e651075
Kassim Sheghembe <sheghembekassim@gmail.com>
2025-02-28 12:24:29 +0300

    Hide register sync temp change (#218) (#220)
    * :hammer: Fixed building issues
    
    * Exclude fhir-path dependency
    
    * Change to provider id sync.
    
    * Hide non RMNCAH register option items.
    
    * Re-arrange registers
    
    * Specify the native folder
    
    ---------
    
    Co-authored-by: cozej4 <ilakozejumanne@gmail.com>

--- opensrp-chw/src/nacp/java/org/smartregister/chw/model/NavigationModelFlv.java
+++ opensrp-chw/src/nacp/java/org/smartregister/chw/model/NavigationModelFlv.java
@@ -71,6 +71,9 @@ public class NavigationModelFlv implements NavigationModel.Flavor {
                     case "iccm_provider":
                         navigationOptions.addAll(Arrays.asList(op10, op20, op8));
                         break;
+                    case "rmncah_provider":
+                        navigationOptions.addAll(Arrays.asList(op10, op1, op3, op5, op2, op25, op8));
+                        break;
                     default:
                         navigationOptions.addAll(Arrays.asList(op10, op1, op11, op12, op3, op5, op2, op13));
                         if (ChwApplication.getApplicationFlavor().hasHIVST()) {
