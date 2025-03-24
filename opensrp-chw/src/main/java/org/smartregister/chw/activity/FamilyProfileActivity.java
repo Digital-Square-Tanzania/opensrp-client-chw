05e671c2da0bbba084dbfed0cb047774868dee77
cozej4 <ilakozejumanne@gmail.com>
2025-03-24 18:12:06 +0300

    :hammer: Code refactoring

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/FamilyProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/FamilyProfileActivity.java
@@ -30,6 +30,7 @@ import org.smartregister.chw.fragment.FamilyProfileActivityFragment;
 import org.smartregister.chw.fragment.FamilyProfileDueFragment;
 import org.smartregister.chw.fragment.FamilyProfileMemberFragment;
 import org.smartregister.chw.hiv.dao.HivDao;
+import org.smartregister.chw.hps.dao.HpsDao;
 import org.smartregister.chw.model.FamilyProfileModel;
 import org.smartregister.chw.pnc.activity.BasePncMemberProfileActivity;
 import org.smartregister.chw.presenter.FamilyProfilePresenter;
@@ -271,7 +272,9 @@ public class FamilyProfileActivity extends CoreFamilyProfileActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        menu.findItem(R.id.action_hps_enrollment).setVisible(ChwApplication.getApplicationFlavor().hasHps());
+        if (ChwApplication.getApplicationFlavor().hasHps()) {
+            menu.findItem(R.id.action_hps_enrollment).setVisible(!HpsDao.isHouseholdRegisteredForHps(familyHead));
+        }
         return true;
     }
 }
