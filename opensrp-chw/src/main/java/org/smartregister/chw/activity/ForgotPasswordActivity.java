fd3c76ca14bfb268ad0d62dc9c14ebe05766a57f
cozej4 <ilakozejumanne@gmail.com>
2025-04-08 11:34:03 +0300

    :hammer: Refactored Login Activity and added Forgot Password to the manifest

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/ForgotPasswordActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/ForgotPasswordActivity.java
@@ -1,4 +1,10 @@
 package org.smartregister.chw.activity;
 
-public class ForgotPasswordActivity {
+import org.smartregister.chw.core.activity.CoreForgotPasswordActivity;
+
+public class ForgotPasswordActivity extends CoreForgotPasswordActivity {
+    @Override
+    public String getFormattedUrl() {
+        return "http://170.187.199.69:3035/api/v1/user/chw/forgot/";
+    }
 }
