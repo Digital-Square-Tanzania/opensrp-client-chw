9f1eecc4b456ea9420bf0b87a4572913cc866fc8
tbwahacker <twaloadam@gmail.com>
2025-05-05 23:53:30 +0300

    Fix issue Rebrand Report Template and bug - fixes #382

--- opensrp-chw/src/nacp/assets/reports/hps_reports/hps-annual-report-script.js
+++ opensrp-chw/src/nacp/assets/reports/hps_reports/hps-annual-report-script.js
@@ -4,6 +4,8 @@ function loadData(reportKey, reportType) {
   const keys = Object.keys(data.nameValuePairs);
   const reportPeriod = document.getElementById("report_period");
   const reportingFacility = document.getElementById("reporting_facility");
+  reportPeriod.innerHTML = Android.getDataPeriod().split("-")[1];
+  reportingFacility.innerHTML = Android.getReportingChw();
   keys.forEach((key) => {
     let element;
     if(reportType!== null && reportType === "pnc"){
@@ -31,6 +33,6 @@ function loadData(reportKey, reportType) {
           tableBody.appendChild(row);
       });
     }
-  reportPeriod.innerHTML = Android.getDataPeriod(reportKey);
-  reportingFacility.innerHTML = Android.getReportingFacility();
+
+
 }
