82c81c454ac02c8c60f414b40f5619cbab5c91eb
Faustina F Honsiah <fhonsiah@gmail.com>
2025-05-12 21:11:55 +0300

    BUG:Fixing Remarks / Maoni field to appear on visit history (#394)
    * modified master library version
    
    * closes #255; BUG: Fix limit multiselect
    
    * closes #263; BUG: Fix client criteria field options to be single selection
    
    * closes #260; Toaster alert for referral system used
    
    * closes #248; BUG: Removing delivery question in registration forms
    
    * closes #247; BUG: Update occupation list in individual clients,household services and household memebers
    
    * closes #254; BUG: Fixing the swahili form titles
    
    * resolved conflict in all clients registration form
    
    * resolved conflict in all clients member relevance
    
    * resolved conflict in family  member relevance
    
    * resolved conflict in family register relevance
    
    * resolved conflicts in family member and family register relevance
    
    * BUG:Preventive services: Fixing the multiselect option,putting titles in every option
    
    * Update master library vesrion in build.gradle
    
    * BUG: Removing exclusive field option in multiselct
    
    * BUG: Removing search hint field in multi-select keys
    
    * BUG: Removing search hint field in multi-select keys
    
    * BUG: Adding search hint field in multi-select keys
    
    * closes #324; BUG: Fixing the number field keys in Huduma Kinga zilizotolewa
    
    * closes #29; BUG:Fixing mobilization session - adding NCD option in IEC materials
    
    * BUG: Fixing the null safety check for number of female and male attended for Mobilization
    
    * closes #341; BUG: Mobilization Session method of education and awareness used
    
    * Created hps_remarks json form
    
    * Modified the library core version
    
    * closes #356; BUG:Modify the list of Preventive services offered by adding food and environment hygiene fields
    
    * closes #33Add Serial Numbers To Lists #377
    
    * BUG: Fixing Referral options
    
    * closes #HPS Referral Service #364
    
    * :hammer: Minor bug fixes
    
    * closes #364; BUG:Fixing Referral services
    
    * closes #338; BUG:Fixing the Remarks/Maoni field to appear in visit history
    
    ---------
    
    Co-authored-by: cozej4 <ilakozejumanne@gmail.com>

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/HpsVisitHistoryActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/HpsVisitHistoryActivity.java
@@ -119,7 +119,7 @@ public class HpsVisitHistoryActivity extends CoreAncMedicalHistoryActivity {
                             "others_type_of_posters", "type_of_message_brochures", "others_type_of_brochures", "number_of_brochures", "preventive_services_provided", "preventive_services",
                             "others_services_specify", "provision_of_curative_services","diseases_signs_and_symptoms","specify_signs_and_symptoms_provided","is_disease_test_conducted",
                             "diseases_test_conducted","systolic","diastolic","malaria_mrdt_result", "blood_sugar_result","hiv_testing_kits_result","body_temperature_result","arm_circumference_result",
-                            "body_weight_result","body_height_result","is_treatment_provided","treatment_provided", "provide_referral_services", "referral_provided", "others_referrals_specify", "referral_system", "referral_system_used", "other_method_specify"};
+                            "body_weight_result","body_height_result","is_treatment_provided","treatment_provided", "provide_referral_services", "referral_provided", "others_referrals_specify", "referral_system", "referral_system_used", "other_method_specify","comments"};
                     extractVisitDetails(visits, visitTypeParams, visitDetails, x, context);
                     hf_visits.add(visitDetails);
 
