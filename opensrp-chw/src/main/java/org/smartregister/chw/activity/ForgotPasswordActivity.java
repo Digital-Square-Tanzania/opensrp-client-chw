07ab908144279bd1ebede93da33d7d2b73b1f84f
cozej4 <ilakozejumanne@gmail.com>
2025-06-06 12:25:46 +0300

    :hammer: refactored forgot password url

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/ForgotPasswordActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/ForgotPasswordActivity.java
@@ -1,10 +1,11 @@
 package org.smartregister.chw.activity;
 
+import org.smartregister.chw.BuildConfig;
 import org.smartregister.chw.core.activity.CoreForgotPasswordActivity;
 
 public class ForgotPasswordActivity extends CoreForgotPasswordActivity {
     @Override
     public String getFormattedUrl() {
-        return "http://170.187.199.69:3035/api/v1/user/chw/forgot/";
+        return BuildConfig.DEBUG ? BuildConfig.reset_password_url_debug : BuildConfig.reset_password_url;
     }
 }
