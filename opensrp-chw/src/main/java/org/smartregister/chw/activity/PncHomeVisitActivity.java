55cb3e08c9ba06ddd297150c7afaa2a7dc25e950
Kassim Sheghembe <sheghembekassim@gmail.com>
2025-07-30 14:14:36 +0300

    Fix notification for PNC

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/PncHomeVisitActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/PncHomeVisitActivity.java
@@ -128,7 +128,7 @@ public class PncHomeVisitActivity extends BasePncHomeVisitActivity {
                                 ReferralUtils.createLinkageTask(org.smartregister.Context.getInstance().allSharedPreferences(),
                                         memberObject.getBaseEntityId(), event.getFormSubmissionId(), minorAilments, org.smartregister.chw.util.Constants.AddoLinkage.PNC_TASK_FOCUS);
 
-                                Toast.makeText(getContext(), getContext().getString(R.string.linked_to_addo_message), Toast.LENGTH_LONG).show();
+                                showToastMessage(getContext().getString(R.string.linked_to_addo_message));
                             }catch (Exception e){
                                 Timber.e(e);
                             }
@@ -146,10 +146,10 @@ public class PncHomeVisitActivity extends BasePncHomeVisitActivity {
                                 String minorAilments = org.smartregister.chw.util.JsonFormUtils.getCheckBoxValue(minorAilmentObject, "child_minor_ailment").toLowerCase();
                                 ReferralUtils.createLinkageTask(org.smartregister.Context.getInstance().allSharedPreferences(),
                                         memberObject.getBaseEntityId(), generateRandomUUIDString(), minorAilments, org.smartregister.chw.util.Constants.AddoLinkage.CHILD_TASK_FOCUS);
-                                Toast.makeText(getContext(), getContext().getString(org.smartregister.chw.R.string.linked_to_addo_message), Toast.LENGTH_LONG).show();
+                                showToastMessage(getContext().getString(org.smartregister.chw.R.string.linked_to_addo_message));
                             }
                             catch (Exception e){
-
+                                Timber.e(e);
                             }
                         }
                     }
@@ -157,6 +157,12 @@ public class PncHomeVisitActivity extends BasePncHomeVisitActivity {
             }
         }
 
+    }
+
+    private void showToastMessage(String message) {
+        if (getContext() != null && getContext() instanceof Activity) {
+            ((Activity) getContext()).runOnUiThread(() -> Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show());
+        }
     }
 
     @Override
