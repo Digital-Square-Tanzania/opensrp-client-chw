9e4c227db6fcd4f802298c033cb75ea4053c8ef7
Kassim Sheghembe <sheghembekassim@gmail.com>
2025-08-20 18:51:29 +0300

    Rule engine helper to calculate Diabetes Risk score.

--- opensrp-chw/src/main/java/org/smartregister/chw/rules/ChwRulesEngineFactory.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/rules/ChwRulesEngineFactory.java
@@ -2,18 +2,27 @@ package org.smartregister.chw.rules;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+
 import com.vijay.jsonwizard.rules.RuleConstant;
 import com.vijay.jsonwizard.rules.RulesEngineFactory;
 
 import org.jeasy.rules.api.Facts;
 import org.jeasy.rules.api.Rule;
+import org.jeasy.rules.api.Rules;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.Map;
+
+import timber.log.Timber;
 
 public class ChwRulesEngineFactory extends RulesEngineFactory {
     private Map<String, String> globalValues;
     private ChwRulesEngineHelper chwRulesEngineHelper;
     private String selectedRuleName;
+    private Rules diabetesRiskRules;
+    private String RULE_FOLDER_PATH = "rule/";
 
 
     public ChwRulesEngineFactory(Context context, Map<String, String> globalValues) {
@@ -41,5 +50,40 @@ public class ChwRulesEngineFactory extends RulesEngineFactory {
     @Override
     public boolean beforeEvaluate(Rule rule, Facts facts) {
         return selectedRuleName != null && selectedRuleName.equals(rule.getName());
+    }
+
+    @Override
+    public String getCalculation(Facts calculationFact, String ruleFilename) {
+        // Special handling for diabetes risk calculation rule (No need to format the calculation result)
+        if (ruleFilename.equals("diabetes_hypertension_screening_calculation.yml")) {
+            Facts facts = this.initializeFacts(calculationFact);
+            facts.put("calculation", "");
+            this.diabetesRiskRules = this.getRulesFromAsset(this.RULE_FOLDER_PATH + ruleFilename);
+            this.processDefaultRules(this.diabetesRiskRules, facts);
+            if (selectedRuleName != null && selectedRuleName.equals("step4_diabetes_risk_score_output"))
+                return facts.get("calculation") != null ? facts.get("calculation").toString() : "0.0";
+            return this.formatCalculationReturnValue(facts.get("calculation"));
+        } else {
+            return super.getCalculation(calculationFact, ruleFilename);
+        }
+    }
+
+    private String formatCalculationReturnValue(Object rawValue) {
+        String value = String.valueOf(rawValue).trim();
+        if (value.isEmpty()) {
+            return "";
+        } else if (rawValue instanceof Map) {
+            return (new JSONObject((Map)rawValue)).toString();
+        } else {
+            if (value.contains(".")) {
+                try {
+                    value = String.valueOf((float)Math.round(Float.valueOf(value) * 100.0F) / 100.0F);
+                } catch (NumberFormatException e) {
+                    Timber.e(e, "%s formatCalculationReturnValue", new Object[]{this.getClass().getCanonicalName()});
+                }
+            }
+
+            return value;
+        }
     }
 }
--- opensrp-chw/src/main/java/org/smartregister/chw/rules/ChwRulesEngineHelper.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/rules/ChwRulesEngineHelper.java
@@ -4,9 +4,17 @@ import com.vijay.jsonwizard.rules.RulesEngineHelper;
 
 import org.smartregister.chw.util.Utils;
 
+import org.smartregister.chw.util.DiabeticRiskCalculator;
+
 public class ChwRulesEngineHelper extends RulesEngineHelper {
 
     public double getWFHZScore(String gender, String height, String weight) {
         return Utils.getWFHZScore(gender, height, weight);
+    }
+
+    public double getDiabesityRiskScore(String age, String waistCircumference, String familyHistory,
+                                        String systolicBloodPressure, String diastolicBloodPressure) {
+        return  DiabeticRiskCalculator.calculateDiabeticRiskScore(age, waistCircumference, familyHistory,
+                systolicBloodPressure, diastolicBloodPressure);
     }
 }
