9f1f94d6cdebe5e68f25219b8e2d3a5e86bd686d
Kassim Sheghembe <sheghembekassim@gmail.com>
2025-09-30 16:02:31 +0300

    Implement a method to open NcdProfileActivity when clicking on the client.

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/NcdRegisterFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/NcdRegisterFragment.java
@@ -1,8 +1,23 @@
 package org.smartregister.chw.fragment;
 
+import android.view.View;
+
+import org.smartregister.chw.R;
+import org.smartregister.chw.activity.NcdProfileActivity;
 import org.smartregister.chw.core.fragment.CoreNcdRegisterFragment;
 
 
 public class NcdRegisterFragment extends CoreNcdRegisterFragment {
 
+    @Override
+    protected void openProfile(String baseEntityId) {
+        NcdProfileActivity.startProfileActivity(getActivity(), baseEntityId);
+    }
+
+    @Override
+    public void setupViews(View view) {
+        super.setupViews(view);
+        View dueOnlyLayout = view.findViewById(R.id.due_only_layout);
+        dueOnlyLayout.setVisibility(View.GONE);
+    }
 }
