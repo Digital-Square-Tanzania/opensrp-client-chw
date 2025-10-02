d8fd5b14bed23cacff1fd30055ac867708142f40
cozej4 <ilakozejumanne@gmail.com>
2025-10-02 05:15:38 +0300

    🐛 Delay TB leprosy profile refresh on resume

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/TbLeprosyProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/TbLeprosyProfileActivity.java
@@ -5,6 +5,8 @@ import static org.smartregister.chw.tbleprosy.util.Constants.JSON_FORM_EXTRA.EVE
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -188,9 +190,19 @@ public class TbLeprosyProfileActivity extends CoreTbLeprosyProfileActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        setupViews();
-        fetchProfileData();
-        profilePresenter.refreshProfileBottom();
+        delayRefresh();
+    }
+
+    private void delayRefresh() {
+        try {
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                setupViews();
+                fetchProfileData();
+                profilePresenter.refreshProfileBottom();
+            }, 500);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
     }
 
     @Override
