0c95b6204c416122fa699c462e16624701532362
cozej4 <ilakozejumanne@gmail.com>
2026-01-19 10:26:20 +0300

    ✨ Add print UI for pre-MAT report

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/HarmReductionPreMatPrintActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/HarmReductionPreMatPrintActivity.java
@@ -1,32 +1,39 @@
 package org.smartregister.chw.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.webkit.WebViewAssetLoader;
 import androidx.webkit.WebViewClientCompat;
 
+import org.smartregister.chw.R;
 import org.smartregister.chw.util.ReportUtils;
 
-import timber.log.Timber;
-
 /**
- * Dedicated activity to render and print the pre-MAT report to avoid context issues
- * when invoking the PrintManager from other screens.
+ * Dedicated activity to render and print the pre-MAT report with an explicit print action.
  */
 public class HarmReductionPreMatPrintActivity extends AppCompatActivity {
     private static final String REPORT_BASE_URL = "https://appassets.androidplatform.net/assets/reports/harmreduction/";
     private static String pendingHtml;
     private static String pendingJobName;
 
+    private WebView webView;
+
     public static void start(AppCompatActivity activity, String htmlContent, String jobName) {
         pendingHtml = htmlContent;
         pendingJobName = jobName;
-        activity.startActivity(new android.content.Intent(activity, HarmReductionPreMatPrintActivity.class));
+        activity.startActivity(new Intent(activity, HarmReductionPreMatPrintActivity.class));
     }
 
     @Override
@@ -36,31 +43,16 @@ public class HarmReductionPreMatPrintActivity extends AppCompatActivity {
             finish();
             return;
         }
+        setContentView(R.layout.activity_reports_view);
+        setupToolbar();
 
-        WebView webView = new WebView(this);
-        setContentView(webView);
-
+        webView = findViewById(R.id.webview);
         WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
                 .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
                 .build();
 
         ReportUtils.setPrintJobName(pendingJobName);
         webView.setWebViewClient(new WebViewClientCompat() {
-            private boolean printed = false;
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                if (!printed) {
-                    printed = true;
-                    try {
-                        ReportUtils.printTheWebPage(view, HarmReductionPreMatPrintActivity.this);
-                    } catch (Exception e) {
-                        Timber.e(e);
-                    }
-                }
-            }
-
             @Override
             public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
                 return assetLoader.shouldInterceptRequest(request.getUrl());
@@ -68,12 +60,47 @@ public class HarmReductionPreMatPrintActivity extends AppCompatActivity {
 
             @Override
             public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
-                return assetLoader.shouldInterceptRequest(android.net.Uri.parse(url));
+                return assetLoader.shouldInterceptRequest(Uri.parse(url));
             }
         });
 
         webView.getSettings().setJavaScriptEnabled(false);
         webView.loadDataWithBaseURL(REPORT_BASE_URL, pendingHtml, "text/html", "UTF-8", null);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(org.smartregister.chw.core.R.id.back_to_nav_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(org.smartregister.chw.core.R.drawable.ic_arrow_back_white_24dp);
+            actionBar.setElevation(0);
+            actionBar.setTitle(R.string.harm_reduction_pre_mat_session_history);
+        }
+        toolbar.setNavigationOnClickListener(v -> finish());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.reports_view_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else if (item.getItemId() == R.id.action_print) {
+            if (webView != null) {
+                ReportUtils.printTheWebPage(webView, this);
+            } else {
+                Toast.makeText(this, R.string.reports_title, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
