2b807d233e75603e50524b15504a98ac78328cb9
cozej4 <ilakozejumanne@gmail.com>
2026-02-18 15:24:25 +0300

    🐛 Set sober house register title in fragment header

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/HarmReductionSoberHouseRegisterFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/HarmReductionSoberHouseRegisterFragment.java
@@ -1,11 +1,24 @@
 package org.smartregister.chw.fragment;
 
+import android.view.View;
+
 import org.smartregister.chw.activity.HarmReductionSoberHouseProfileActivity;
+import org.smartregister.chw.core.R;
 import org.smartregister.chw.core.fragment.CoreHarmReductionRegisterFragment;
 import org.smartregister.chw.core.model.CoreHarmReductionRegisterFragmentModel;
 import org.smartregister.chw.presenter.HarmReductionSoberHouseRegisterFragmentPresenter;
+import org.smartregister.view.customcontrols.CustomFontTextView;
 
 public class HarmReductionSoberHouseRegisterFragment extends CoreHarmReductionRegisterFragment {
+
+    @Override
+    public void setupViews(View view) {
+        super.setupViews(view);
+        CustomFontTextView titleView = view.findViewById(R.id.txt_title_label);
+        if (titleView != null) {
+            titleView.setText(org.smartregister.chw.harmreduction.R.string.record_sober_house_services);
+        }
+    }
 
     @Override
     protected void openProfile(String baseEntityId) {
