4745fc4ce96d20d575f9c4b47f481a54ce08f9b8
Kassim Sheghembe <sheghembekassim@gmail.com>
2026-02-19 12:23:49 +0300

    ✅ Add tests for existing household head prefills

--- opensrp-chw/src/test/java/org/smartregister/chw/util/JsonFormUtilsTest.java
+++ opensrp-chw/src/test/java/org/smartregister/chw/util/JsonFormUtilsTest.java
@@ -1,11 +1,26 @@
 package org.smartregister.chw.util;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import com.vijay.jsonwizard.constants.JsonFormConstants;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.family.util.DBConstants;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 28)
 public class JsonFormUtilsTest {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
 
     @Test
     public void getTimeZone() {
@@ -24,5 +39,73 @@ public class JsonFormUtilsTest {
             e.printStackTrace();
         }
         Assert.assertEquals("Yes", JsonFormUtils.getValue(jsonObject, key));
+    }
+
+    @Test
+    public void testPopulateExistingHeadSetsReadOnlyFields() throws Exception {
+        JSONObject form = new JSONObject();
+        form.put(JsonFormUtils.METADATA, new JSONObject());
+
+        JSONObject stepTwo = new JSONObject();
+        JSONArray fields = new JSONArray();
+        stepTwo.put(JsonFormConstants.FIELDS, fields);
+        form.put(org.smartregister.family.util.JsonFormUtils.STEP2, stepTwo);
+
+        addField(fields, "existing_head");
+        addField(fields, "first_name");
+        addField(fields, "middle_name");
+        addField(fields, "surname");
+        addField(fields, DBConstants.KEY.DOB);
+        addField(fields, "age");
+        addField(fields, "sex");
+        addField(fields, "unique_id");
+        addField(fields, "phone_number");
+        addField(fields, "other_phone_number");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(JsonFormConstants.VALUE, "");
+
+        java.util.HashMap<String, String> columnMap = new java.util.HashMap<>();
+        columnMap.put(DBConstants.KEY.FIRST_NAME, "Jane");
+        columnMap.put(DBConstants.KEY.MIDDLE_NAME, "M");
+        columnMap.put(DBConstants.KEY.LAST_NAME, "Doe");
+        columnMap.put(DBConstants.KEY.GENDER, "Female");
+        columnMap.put(DBConstants.KEY.DOB, "1990-01-01");
+        columnMap.put(DBConstants.KEY.UNIQUE_ID, "12345");
+        columnMap.put(DBConstants.KEY.PHONE_NUMBER, "0712345678");
+        columnMap.put(DBConstants.KEY.OTHER_PHONE_NUMBER, "0799999999");
+
+        CommonPersonObjectClient client = new CommonPersonObjectClient("case-id", columnMap, "Jane Doe");
+        client.setColumnmaps(columnMap);
+
+        JsonFormUtils.populateExistingHead(form, client);
+
+        JSONObject uniqueIdField = findField(fields, "unique_id");
+        Assert.assertEquals("12345", uniqueIdField.optString(JsonFormConstants.VALUE));
+        Assert.assertEquals("true", uniqueIdField.optString(JsonFormUtils.READ_ONLY));
+
+        JSONObject existingHeadField = findField(fields, "existing_head");
+        Assert.assertEquals("case-id", existingHeadField.optString(JsonFormConstants.VALUE));
+
+        JSONObject phoneField = findField(fields, "phone_number");
+        Assert.assertEquals("0712345678", phoneField.optString(JsonFormConstants.VALUE));
+        Assert.assertEquals("", phoneField.optString(JsonFormUtils.READ_ONLY));
+    }
+
+    private JSONObject addField(JSONArray fields, String key) throws JSONException {
+        JSONObject field = new JSONObject();
+        field.put(JsonFormUtils.KEY, key);
+        fields.put(field);
+        return field;
+    }
+
+    private JSONObject findField(JSONArray fields, String key) {
+        for (int i = 0; i < fields.length(); i++) {
+            JSONObject obj = fields.optJSONObject(i);
+            if (obj != null && key.equals(obj.optString(JsonFormUtils.KEY))) {
+                return obj;
+            }
+        }
+        return null;
     }
 }
