a5951419a1ed4c7f2deb34f65e4f99471ee74ce0
cozej4 <ilakozejumanne@gmail.com>
2026-02-19 16:01:22 +0300

    🐛 Start sober house form with enrollment title

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/HarmReductionSoberHouseRegisterActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/HarmReductionSoberHouseRegisterActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import androidx.annotation.MenuRes;
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONObject;
 import org.smartregister.chw.R;
 import org.smartregister.chw.core.activity.CoreHarmReductionRegisterActivity;
 import org.smartregister.chw.core.custom_views.NavigationMenu;
+import org.smartregister.chw.core.utils.FormUtils;
 import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.chw.fragment.HarmReductionSoberHouseRegisterFragment;
 import org.smartregister.chw.harmreduction.util.Constants;
@@ -39,6 +41,16 @@ public class HarmReductionSoberHouseRegisterActivity extends CoreHarmReductionRe
     @MenuRes
     public int getMenuResource() {
         return R.menu.bottom_nav_harm_reduction_sober_house;
+    }
+
+    @Override
+    public void startFormActivity(JSONObject jsonForm) {
+        Intent intent = FormUtils.getStartFormActivity(
+                jsonForm,
+                getString(R.string.harm_reduction_sober_house_enrollment),
+                this
+        );
+        startActivityForResult(intent, Constants.REQUEST_CODE_GET_JSON);
     }
 
     @Override
