6c5f4e3eeea5e7793daa0a1b37f6e7a37907e76f
cozej4 <ilakozejumanne@gmail.com>
2026-02-19 13:20:50 +0300

    🐛 Wire sober house close job into scheduler and job creator

--- opensrp-chw/src/main/java/org/smartregister/chw/job/ChwJobCreator.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/job/ChwJobCreator.java
@@ -12,6 +12,7 @@ import org.smartregister.chw.core.job.HomeVisitServiceJob;
 import org.smartregister.chw.core.job.StockUsageReportJob;
 import org.smartregister.chw.core.job.SyncTaskWithClientEventsServiceJob;
 import org.smartregister.chw.core.job.VaccineRecurringServiceJob;
+import org.smartregister.chw.harmreduction.job.CloseSoberHouseMemberServiceJob;
 import org.smartregister.chw.core.sync.intent.SyncClientEventsPerTaskIntentService;
 import org.smartregister.chw.sync.ChwSyncIntentService;
 import org.smartregister.chw.sync.intent.ChwSyncTaskIntentService;
@@ -72,6 +73,8 @@ public class ChwJobCreator implements JobCreator {
                 return new PncCloseDateServiceJob();
             case CloseAsrhMemberServiceJob.TAG:
                 return new CloseAsrhMemberServiceJob();
+            case CloseSoberHouseMemberServiceJob.TAG:
+                return new CloseSoberHouseMemberServiceJob();
             //TODO uncomment to enable plans
             /*case PlanIntentServiceJob.TAG:
                 return new PlanIntentServiceJob();*/
