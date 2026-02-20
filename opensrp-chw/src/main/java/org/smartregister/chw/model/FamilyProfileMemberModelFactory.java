5e716eae014f764fa7e227053f1da4b001c5e87d
Kassim Sheghembe <sheghembekassim@gmail.com>
2026-02-20 12:22:18 +0300

    build: avoid duplicate factory by using single main factory with flavor switch
    - Main factory now uses BuildConfig.FLAVOR + reflection to instantiate nacp model
    - Remove nacp factory class to prevent duplicate class error

--- opensrp-chw/src/main/java/org/smartregister/chw/model/FamilyProfileMemberModelFactory.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/model/FamilyProfileMemberModelFactory.java
@@ -1,10 +1,18 @@
 package org.smartregister.chw.model;
 
+import org.smartregister.chw.BuildConfig;
 import org.smartregister.chw.core.model.CoreFamilyProfileMemberModel;
 
 public class FamilyProfileMemberModelFactory {
     public static CoreFamilyProfileMemberModel create() {
+        if ("nacp".equalsIgnoreCase(BuildConfig.FLAVOR)) {
+            try {
+                Class<?> clazz = Class.forName("org.smartregister.chw.model.FamilyProfileMemberModelNacp");
+                return (CoreFamilyProfileMemberModel) clazz.newInstance();
+            } catch (Exception ignored) {
+                // fall through to default
+            }
+        }
         return new FamilyProfileMemberModel();
     }
 }
-
