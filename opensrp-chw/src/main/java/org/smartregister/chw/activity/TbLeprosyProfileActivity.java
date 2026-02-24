c1500d7383ed5a1b0e24a568ee0482dd0611809b
cozej4 <ilakozejumanne@gmail.com>
2026-02-24 15:28:59 +0300

    🐛 Treat blank leprosy client numbers as missing

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/TbLeprosyProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/TbLeprosyProfileActivity.java
@@ -179,7 +179,7 @@ public class TbLeprosyProfileActivity extends CoreTbLeprosyProfileActivity imple
                     || StringUtils.isNotBlank(observationResults.getClinicalDecision())
                     || StringUtils.isNotBlank(observationResults.getLeprosyInvestigationResults()));
             boolean missingLeprosyTreatmentStartDate = observationResults != null
-                    && StringUtils.isBlank(observationResults.getLeprosyTreatmentStartDate()) && memberObject.getLeprosyClientNumber() == null;
+                    && StringUtils.isBlank(observationResults.getLeprosyTreatmentStartDate()) && StringUtils.isBlank(memberObject.getLeprosyClientNumber());
             boolean hasPoorQualitySample = observationResults != null && observationResults.isPoorQualitySample();
             boolean hasTbResults = observationResults != null && (StringUtils.isNotBlank(observationResults.getTbSampleTestResults())
                     || StringUtils.isNotBlank(observationResults.getClinicalDecision()));
