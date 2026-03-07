374afa99857e2260f75426bd1474a70602bc4606
maulidkategile <maulidkategile@gmail.com>
2026-03-07 17:17:36 +0300

    hook global home-visit processing to trigger iCCM referral auto-processing

--- opensrp-chw/src/main/java/org/smartregister/chw/job/ChwJobCreator.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/job/ChwJobCreator.java
@@ -51,7 +51,7 @@ public class ChwJobCreator implements JobCreator {
             case P2pServiceJob.TAG:
                 return new P2pServiceJob();
             case HomeVisitServiceJob.TAG:
-                return new HomeVisitServiceJob();
+                return new ChwHomeVisitServiceJob();
             case ChwIndicatorGeneratingJob.TAG:
                 return new ChwIndicatorGeneratingJob();
             case BasePncCloseJob.TAG:
