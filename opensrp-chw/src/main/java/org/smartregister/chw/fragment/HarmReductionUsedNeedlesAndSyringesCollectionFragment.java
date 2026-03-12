88dd4203913122acecea1e66ea3988bc0e46134d
cozej4 <ilakozejumanne@gmail.com>
2026-03-12 08:29:15 +0300

    ✨ Add or update features in opensrp-chw

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/HarmReductionUsedNeedlesAndSyringesCollectionFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/HarmReductionUsedNeedlesAndSyringesCollectionFragment.java
@@ -4,13 +4,19 @@ import android.view.View;
 
 import org.json.JSONObject;
 import org.smartregister.chw.R;
+import org.smartregister.chw.adapter.HarmReductionUsedNeedlesAndSyringesCollectionRegisterAdapter;
 import org.smartregister.chw.core.custom_views.NavigationMenu;
+import org.smartregister.chw.harmreduction.dao.HarmReductionUsedNeedlesAndSyringesCollectionDao;
 import org.smartregister.chw.harmreduction.fragment.BaseHarmReductionUsedNeedlesAndSyringesCollectionRegisterFragment;
+import org.smartregister.chw.harmreduction.model.HarmReductionUsedNeedlesAndSyringesCollectionModel;
 import org.smartregister.chw.util.JsonFormUtils;
+
+import java.util.List;
 
 import timber.log.Timber;
 
 public class HarmReductionUsedNeedlesAndSyringesCollectionFragment extends BaseHarmReductionUsedNeedlesAndSyringesCollectionRegisterFragment {
+    private HarmReductionUsedNeedlesAndSyringesCollectionRegisterAdapter adapter;
 
     @Override
     public void setupViews(View view) {
@@ -38,5 +44,30 @@ public class HarmReductionUsedNeedlesAndSyringesCollectionFragment extends BaseH
                 ),
                 JsonFormUtils.REQUEST_CODE_GET_JSON
         );
+    }
+
+    @Override
+    protected void setUpAdapter() {
+        List<HarmReductionUsedNeedlesAndSyringesCollectionModel> collectionModels = HarmReductionUsedNeedlesAndSyringesCollectionDao.getCollectionSessions();
+        if (collectionModels != null && !collectionModels.isEmpty()) {
+            adapter = new HarmReductionUsedNeedlesAndSyringesCollectionRegisterAdapter(collectionModels, requireActivity());
+            clientsView.setAdapter(adapter);
+        } else {
+            adapter = null;
+            clientsView.setAdapter(null);
+        }
+
+        showEmptyState();
+    }
+
+    @Override
+    protected void showEmptyState() {
+        if (emptyViewLayout != null) {
+            if (adapter != null && adapter.getItemCount() >= 1) {
+                emptyViewLayout.setVisibility(View.GONE);
+            } else {
+                emptyViewLayout.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
