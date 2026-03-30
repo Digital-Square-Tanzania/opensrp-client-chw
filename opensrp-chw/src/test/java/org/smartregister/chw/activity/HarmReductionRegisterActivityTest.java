a6912a6e84d4ee9fe73ba6cc0f9426cad56ca44e
cozej4 <ilakozejumanne@gmail.com>
2026-03-30 10:44:50 +0300

    fix: add harm reduction registration chooser

--- opensrp-chw/src/test/java/org/smartregister/chw/activity/HarmReductionRegisterActivityTest.java
+++ opensrp-chw/src/test/java/org/smartregister/chw/activity/HarmReductionRegisterActivityTest.java
@@ -1,5 +1,7 @@
 package org.smartregister.chw.activity;
 
+import android.content.Intent;
+
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
@@ -32,5 +34,18 @@ public class HarmReductionRegisterActivityTest extends BaseUnitTest {
                         )));
 
         Assert.assertFalse(HarmReductionRegisterActivity.shouldLaunchPreMatSession(form));
+    }
+
+    @Test
+    public void resolveRegistrationFormNameReturnsExistingClientFormForExistingClientChoice() {
+        Assert.assertEquals(HarmReductionRegisterActivity.HARM_REDUCTION_REGISTER_EXISTING_CLIENT_FORM,
+                HarmReductionRegisterActivity.resolveRegistrationFormName(1));
+    }
+
+    @Test
+    public void shouldShowRegistrationChooserReturnsTrueWhenLaunchFlagIsSet() {
+        Intent intent = new Intent().putExtra(HarmReductionRegisterActivity.EXTRA_SHOW_REGISTRATION_CHOOSER, true);
+
+        Assert.assertTrue(HarmReductionRegisterActivity.shouldShowRegistrationChooser(intent));
     }
 }
