7f307c597c0434d6936bf880d3fe607bfedb097a
cozej4 <ilakozejumanne@gmail.com>
2026-04-16 17:08:41 +0300

    :hammer: code refactoring

--- opensrp-chw/src/main/java/org/smartregister/chw/actionhelper/ToddlerDangerSignsBabyHelper.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/actionhelper/ToddlerDangerSignsBabyHelper.java
@@ -36,9 +36,6 @@ public class ToddlerDangerSignsBabyHelper extends HomeVisitActionHelper {
 
     @Override
     public BaseAncHomeVisitAction.ScheduleStatus getPreProcessedStatus() {
-        if (alert == null) {
-            return null;
-        }
         return isOverDue() ? BaseAncHomeVisitAction.ScheduleStatus.OVERDUE : BaseAncHomeVisitAction.ScheduleStatus.DUE;
     }
 
