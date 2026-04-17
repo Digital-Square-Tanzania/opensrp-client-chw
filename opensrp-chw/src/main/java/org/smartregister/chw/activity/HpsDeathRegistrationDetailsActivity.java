8cf20b5751276f210a28a63aa6e6ca8f2de7c914
lodyne <lodgmtui@gmail.com>
2026-04-17 12:38:39 +0300

    🐛 fix(ui): show specified cause in death summary
    ~ Replace other with the entered cause in summary output
    ~ Remove the auxiliary specify field from displayed visit details
    ~ Add unit coverage for summary cause mapping

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/HpsDeathRegistrationDetailsActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/HpsDeathRegistrationDetailsActivity.java
@@ -42,6 +42,7 @@ import org.smartregister.chw.core.utils.CoreReferralUtils;
 import org.smartregister.chw.core.utils.FormUtils;
 import org.smartregister.chw.hps.util.Constants;
 import org.smartregister.chw.interactor.HpsDeathRegistrationDetailsInteractor;
+import org.smartregister.chw.util.HpsDeathRegistrationSummaryUtil;
 import org.smartregister.clientandeventmodel.Event;
 import org.smartregister.clientandeventmodel.Obs;
 import org.smartregister.family.util.JsonFormUtils;
@@ -169,8 +170,7 @@ import timber.log.Timber;
                     String[] healthEducationMobilizationLocation = {"dod", "first_name", "middle_name", "last_name", "dob", "sex", "cause_of_death", "cause_of_death_specify", "comments"};
                     extractVisitDetails(visits, healthEducationMobilizationLocation, visitDetails, x, context);
 
-
-                    hf_visits.add(visitDetails);
+                    hf_visits.add(HpsDeathRegistrationSummaryUtil.getDisplayValues(visitDetails, context.getString(R.string.hps_other_reason)));
 
                     x++;
                 }
