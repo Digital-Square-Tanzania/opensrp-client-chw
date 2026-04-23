df6f7d7b93fc784d7727653243061ff9ed58e202
Kassim Sheghembe <sheghembekassim@gmail.com>
2026-04-23 16:34:08 +0300

    ✨ Add NCD NACP-specific wizard activities and form fragments

---
+++ opensrp-chw/src/nacp/java/org/smartregister/chw/fragment/NcdJsonWizardFormFragment.java
@@ -0,0 +1,103 @@
+package org.smartregister.chw.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+import com.vijay.jsonwizard.fragments.JsonWizardFormFragment;
+import com.vijay.jsonwizard.interactors.JsonFormInteractor;
+import com.vijay.jsonwizard.presenters.JsonFormFragmentPresenter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.chw.R;
+import org.smartregister.chw.presenter.NcdJsonWizardFormFragmentPresenter;
+
+import timber.log.Timber;
+
+public class NcdJsonWizardFormFragment extends JsonWizardFormFragment {
+
+    public static NcdJsonWizardFormFragment getFormFragment(String stepName) {
+        NcdJsonWizardFormFragment jsonFormFragment = new NcdJsonWizardFormFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(com.vijay.jsonwizard.constants.JsonFormConstants.JSON_FORM_KEY.STEPNAME, stepName);
+        jsonFormFragment.setArguments(bundle);
+        return jsonFormFragment;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+/*    @Override
+    public void onResume() {
+        super.onResume();
+        if (!getJsonApi().isPreviousPressed()) {
+            skipStepsOnNextPressed();
+        }
+    }*/
+
+    /**
+     * Skips blank by relevance steps when next is clicked on the json wizard forms.
+     */
+    public void skipStepsOnNextPressed() {
+        if (skipBlankSteps()) {
+            JSONObject formStep = getStep(getArguments().getString(JsonFormConstants.STEPNAME));
+            String next = formStep.optString(JsonFormConstants.NEXT, "");
+            if (StringUtils.isNotEmpty(next)) {
+                checkIfStepIsBlank(formStep);
+                if (shouldSkipStep()) {
+                    next();
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if a given step is blank due to relevance hidding all the widgets
+     *
+     * @param formStep {@link JSONObject}
+     */
+    private void checkIfStepIsBlank(JSONObject formStep) {
+        try {
+            if (formStep.has(JsonFormConstants.FIELDS)) {
+                JSONArray fields = formStep.getJSONArray(JsonFormConstants.FIELDS);
+                for (int i = 0; i < fields.length(); i++) {
+                    JSONObject field = fields.getJSONObject(i);
+                    if (field.has(JsonFormConstants.TYPE) && !JsonFormConstants.HIDDEN.equals(field.getString(JsonFormConstants.TYPE))) {
+                        boolean isVisible = field.optBoolean(JsonFormConstants.IS_VISIBLE, true);
+                        if (isVisible) {
+                            setShouldSkipStep(false);
+                            break;
+                        }
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            Timber.e(e, "%s --> checkIfStepIsBlank", this.getClass().getCanonicalName());
+        }
+    }
+
+    @Override
+    protected JsonFormFragmentPresenter createPresenter() {
+        return new NcdJsonWizardFormFragmentPresenter(this, JsonFormInteractor.getInstance());
+    }
+
+    public void customClick(Context context, String behaviour){
+        save();
+    }
+    @Override
+    public void updateVisibilityOfNextAndSave(boolean next, boolean save) {
+        super.updateVisibilityOfNextAndSave(next, save);
+        final String nextStep = getJsonApi().nextStep();
+        if ("step4".equals(nextStep)) {
+            getMenu().findItem(R.id.action_save).setVisible(false);
+        }
+    }
+}
---
+++ opensrp-chw/src/nacp/java/org/smartregister/chw/presenter/NcdJsonWizardFormFragmentPresenter.java
@@ -0,0 +1,124 @@
+package org.smartregister.chw.presenter;
+
+import android.content.Intent;
+import android.widget.LinearLayout;
+
+import com.vijay.jsonwizard.fragments.JsonFormFragment;
+import com.vijay.jsonwizard.interactors.JsonFormInteractor;
+import com.vijay.jsonwizard.presenters.JsonWizardFormFragmentPresenter;
+import com.vijay.jsonwizard.utils.FormUtils;
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+import com.vijay.jsonwizard.views.JsonFormFragmentView;
+import com.vijay.jsonwizard.viewstates.JsonFormFragmentViewState;
+
+import org.smartregister.chw.fragment.NcdJsonWizardFormFragment;
+
+public class NcdJsonWizardFormFragmentPresenter extends JsonWizardFormFragmentPresenter {
+
+    private FormUtils formUtils = new FormUtils();
+    public NcdJsonWizardFormFragmentPresenter(JsonFormFragment jsonFormFragment, JsonFormInteractor jsonFormInteractor) {
+        super(jsonFormFragment, jsonFormInteractor);
+    }
+
+
+    @Override
+    public boolean onNextClick(LinearLayout mainView) {
+        validateAndWriteValues();
+        checkAndStopCountdownAlarm();
+        boolean validateOnSubmit = validateOnSubmit();
+        if (validateOnSubmit && getIncorrectlyFormattedFields().isEmpty()) {
+            boolean isSkipped = this.executeRefreshLogicForNextStep();
+            return !isSkipped && moveToNextWizardStep();
+        } else if (isFormValid()) {
+            boolean isSkipped = this.executeRefreshLogicForNextStep();
+            return !isSkipped && moveToNextWizardStep();
+        } else {
+            getView().showSnackBar(getView().getContext().getResources()
+                    .getString(com.vijay.jsonwizard.R.string.json_form_on_next_error_msg));
+        }
+        return false;
+    }
+
+    @Override
+    public boolean executeRefreshLogicForNextStep() {
+        boolean isSkipped = false;
+        final String nextStep = getFormFragment().getJsonApi().nextStep();
+        if (org.apache.commons.lang3.StringUtils.isNotBlank(nextStep)) {
+            getmJsonFormInteractor().fetchFormElements(nextStep, getFormFragment(), getFormFragment().getJsonApi().getmJSONObject().optJSONObject(nextStep), getView().getCommonListener(), false);
+            getFormFragment().getJsonApi().initializeDependencyMaps();
+            getFormFragment().getJsonApi().setNextStepRelevant(false);
+            getFormFragment().getJsonApi().invokeRefreshLogic(null, false, null, null, nextStep, true);
+            if (!getFormFragment().getJsonApi().isNextStepRelevant()) {
+                com.vijay.jsonwizard.utils.Utils.checkIfStepHasNoSkipLogic(getFormFragment());
+                // Clear data for skipped step
+                clearStepData(nextStep);
+            }
+            isSkipped = getFormFragment().skipStepsOnNextPressed(nextStep);
+        }
+        return isSkipped;
+    }
+
+    /**
+     * Clears user-input values for all fields in a given step, but keeps the field definitions
+     */
+    private void clearStepData(String stepName) {
+        try {
+            org.json.JSONObject formJson = getFormFragment().getJsonApi().getmJSONObject();
+            if (formJson != null && formJson.has(stepName)) {
+                org.json.JSONObject stepObj = formJson.optJSONObject(stepName);
+                if (stepObj != null && stepObj.has("fields")) {
+                    org.json.JSONArray fields = stepObj.optJSONArray("fields");
+                    if (fields != null) {
+                        for (int i = 0; i < fields.length(); i++) {
+                            org.json.JSONObject field = fields.optJSONObject(i);
+                            if (field != null) {
+                                getView().writeValue(stepName, field.optString("key"), "",
+                                        field.optString("openmrs_entity_parent", ""),
+                                        field.optString("openmrs_entity", ""),
+                                        field.optString("openmrs_entity_id", ""),
+                                        field.optBoolean("popup", false));
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            timber.log.Timber.e(e, "Error clearing values for step: %s", stepName);
+        }
+    }
+
+    @Override
+    public void onSaveClick(LinearLayout mainView) {
+        validateAndWriteValues();
+        checkAndStopCountdownAlarm();
+        boolean isFormValid = isFormValid();
+        if (isFormValid || Boolean.valueOf(mainView.getTag(com.vijay.jsonwizard.R.id.skip_validation).toString())) {
+            Intent returnIntent = new Intent();
+            getView().onFormFinish();
+            returnIntent.putExtra("json", formUtils.addFormDetails(getView().getCurrentJsonState()));
+            returnIntent.putExtra(JsonFormConstants.SKIP_VALIDATION,
+                    Boolean.valueOf(mainView.getTag(com.vijay.jsonwizard.R.id.skip_validation).toString()));
+            getView().finishWithResult(returnIntent);
+        } else {
+            if (showErrorsOnSubmit()) {
+                launchErrorDialog();
+                getView().showToast(getView().getContext().getResources()
+                        .getString(com.vijay.jsonwizard.R.string.json_form_error_msg, getInvalidFields().size()));
+            } else {
+                getView().showSnackBar(getView().getContext().getResources()
+                        .getString(com.vijay.jsonwizard.R.string.json_form_error_msg, getInvalidFields().size()));
+
+            }
+        }
+    }
+
+    protected boolean moveToNextWizardStep() {
+        final String nextStep = getFormFragment().getJsonApi().nextStep();
+        if (!"".equals(nextStep)) {
+            JsonFormFragment next = NcdJsonWizardFormFragment.getFormFragment(nextStep);
+            getView().hideKeyBoard();
+            getView().transactThis(next);
+        }
+        return false;
+    }
+}
