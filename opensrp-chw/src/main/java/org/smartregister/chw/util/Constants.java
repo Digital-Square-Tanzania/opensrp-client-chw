fc2ca7471b12e2b001d17f9e11be339bfd894b05
devcolonel <dev.marioanthony@gmail.com>
2026-05-14 05:53:34 +0300

    ✨ chore: support location info visibility for independent clients
    Add `ENTITY_TYPE_EC_INDEPENDENT_CLIENT` constant and update `AllClientsUtils` to conditionally show the location info menu item based on the client's entity type.
    
    - ⚙️ **Constants**: add `ENTITY_TYPE_EC_INDEPENDENT_CLIENT` string
    - 🛠️ **AllClientsUtils**: update `setupMenu` logic to only display `action_location_info` if the entity type matches an independent client

--- opensrp-chw/src/main/java/org/smartregister/chw/util/Constants.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/Constants.java
@@ -35,6 +35,7 @@ public class Constants extends CoreConstants {
     public static String ENABLE_DATE_RANGE_FILTER = "ENABLE_DATE_RANGE_FILTER";
     public static String ENTITY_TYPE_EC_FAMILY_MEMBER = "ec_family_member";
     public static String ENTITY_TYPE_EC_FAMILY = "ec_family";
+    public static String ENTITY_TYPE_EC_INDEPENDENT_CLIENT = "ec_independent_client";
 
     public static int REQUEST_FILTERS = 2004;
 
