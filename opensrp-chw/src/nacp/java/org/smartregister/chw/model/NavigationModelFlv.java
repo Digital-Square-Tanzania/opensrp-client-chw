96745287bbfc31dd4e96a06158827ab0f0c32f40
cozej4 <ilakozejumanne@gmail.com>
2026-05-29 17:07:37 +0300

    ✨ Enable AYP out-of-school role access

--- opensrp-chw/src/nacp/java/org/smartregister/chw/model/NavigationModelFlv.java
+++ opensrp-chw/src/nacp/java/org/smartregister/chw/model/NavigationModelFlv.java
@@ -80,6 +80,9 @@ public class NavigationModelFlv implements NavigationModel.Flavor {
                     case "rmncah_provider":
                         navigationOptions.addAll(Arrays.asList(op10, op1, op3, op5, op2, op25, op8, op9));
                         break;
+                    case "AYP_OUT_OF_SCHOOL":
+                        navigationOptions.addAll(Arrays.asList(op10, op28, op9, op8));
+                        break;
                     default:
                         navigationOptions.addAll(Arrays.asList(op10, op1, op11, op12, op3, op5, op2, op13));
                         if (ChwApplication.getApplicationFlavor().hasHIVST()) {
