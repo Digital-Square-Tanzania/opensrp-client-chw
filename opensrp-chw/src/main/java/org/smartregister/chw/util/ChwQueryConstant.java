d87f85e35de631d71ed105ed61bbf3005ea55234
ChrissDisigale <chrispro.genius@gmail.com>
2026-06-06 20:37:36 +0300

    change independent to mother mentor

--- opensrp-chw/src/main/java/org/smartregister/chw/util/ChwQueryConstant.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/ChwQueryConstant.java
@@ -81,6 +81,10 @@ public interface ChwQueryConstant {
             "    FROM ec_ayp_out_school_enrollment\n" +
             "    WHERE is_closed is 0 \n" +
             "    UNION ALL\n" +
+            "    SELECT ec_mothermentor_enrollment.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_mothermentor_enrollment\n" +
+            "    WHERE ec_mothermentor_enrollment.is_closed is 0 AND (ec_mothermentor_enrollment.status IS NULL OR ec_mothermentor_enrollment.status = 'client') AND ec_mothermentor_enrollment.screening_status != '-' \n" +
+            "    UNION ALL\n" +
             "    SELECT ec_hps_client_register.base_entity_id AS base_entity_id\n" +
             "    FROM ec_hps_client_register\n" +
             "    WHERE is_closed is 0 AND does_the_client_consent_to_be_enrolled_in_hps_services = 'yes' \n" +
@@ -233,6 +237,10 @@ public interface ChwQueryConstant {
             "    UNION ALL\n" +
             "    SELECT ec_cbhs_register.base_entity_id AS base_entity_id\n" +
             "    FROM ec_cbhs_register\n" +
+            "    UNION ALL\n" +
+            "    SELECT ec_mothermentor_enrollment.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_mothermentor_enrollment\n" +
+            "    WHERE ec_mothermentor_enrollment.is_closed is 0 AND (ec_mothermentor_enrollment.status IS NULL OR ec_mothermentor_enrollment.status = 'client') AND ec_mothermentor_enrollment.screening_status != '-' \n" +
             ")\n" +
             "UNION ALL\n" +
             "/* ANC REGISTER */\n" +
@@ -793,6 +801,56 @@ public interface ChwQueryConstant {
             "where ec_family_member.date_removed is null\n" +
             "  AND ec_ayp_out_school_enrollment.is_closed is 0\n" +
 //            "  AND ec_ayp_out_school_enrollment.should_enroll = 'yes'\n" +
+            "  AND ec_family_member.base_entity_id IN (%s)\n" +
+            "  AND ec_family_member.base_entity_id NOT IN (\n" +
+            "    SELECT ec_anc_register.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_anc_register\n" +
+            "    UNION ALL\n" +
+            "    SELECT ec_kvp_prep_register.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_kvp_prep_register where ec_kvp_prep_register.is_closed is 0\n" +
+            "    UNION ALL\n" +
+            "    SELECT ec_pregnancy_outcome.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_pregnancy_outcome\n" +
+            "    UNION ALL\n" +
+            "    SELECT ec_child.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_child\n" +
+            "    UNION ALL\n" +
+            "    SELECT ec_malaria_confirmation.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_malaria_confirmation\n" +
+            "    UNION ALL\n" +
+            "    SELECT ec_sbc_register.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_sbc_register\n" +
+            "    UNION ALL\n" +
+            "    SELECT ec_tb_register.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_tb_register\n" +
+            "    WHERE ec_tb_register.tb_case_closure_date is null\n" +
+            "    UNION ALL\n" +
+            "    SELECT ec_cbhs_register.base_entity_id AS base_entity_id\n" +
+            "    FROM ec_cbhs_register)\n" +
+            "UNION ALL\n" +
+            "\n" +
+            "/*ONLY Mother Mentor clients*/\n" +
+            "SELECT ec_family_member.first_name,\n" +
+            "       ec_family_member.middle_name,\n" +
+            "       ec_family_member.last_name,\n" +
+            "       ec_family_member.gender,\n" +
+            "       ec_family_member.dob,\n" +
+            "       ec_family_member.base_entity_id,\n" +
+            "       ec_family_member.id                          as _id,\n" +
+            "       ec_family_member.entity_type,\n" +
+            "       'Mother Mentor'                             AS register_type,\n" +
+            "       ec_family_member.relational_id               as relationalid,\n" +
+            "       ec_family.village_town                       as home_address,\n" +
+            "       NULL                                         AS mother_first_name,\n" +
+            "       NULL                                         AS mother_last_name,\n" +
+            "       NULL                                         AS mother_middle_name,\n" +
+            "       ec_mothermentor_enrollment.last_interacted_with AS last_interacted_with\n" +
+            "FROM ec_family_member\n" +
+            "         inner join ec_family on ec_family.base_entity_id = ec_family_member.relational_id\n" +
+            "         inner join ec_mothermentor_enrollment\n" +
+            "                    on ec_family_member.base_entity_id = ec_mothermentor_enrollment.base_entity_id\n" +
+            "where ec_family_member.date_removed is null\n" +
+            "  AND ec_mothermentor_enrollment.is_closed is 0 AND (ec_mothermentor_enrollment.status IS NULL OR ec_mothermentor_enrollment.status = 'client') AND ec_mothermentor_enrollment.screening_status != '-' \n" +
             "  AND ec_family_member.base_entity_id IN (%s)\n" +
             "  AND ec_family_member.base_entity_id NOT IN (\n" +
             "    SELECT ec_anc_register.base_entity_id AS base_entity_id\n" +
